6eabddde0d3c867fb8634ef0b1b425ac5edc3ff9
Michael Wolf <susonicth@gmail.com>
2025-04-17 01:45:42 +0200

    benchmark against arrayList

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zigStringUtil", .{
-        .source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -18,4 +18,14 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+
+    const benchmark_tests = b.addTest(.{
+        .root_source_file = b.path("src/Benchmark.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_benchmark_tests = b.addRunArtifact(benchmark_tests);
+
+    const benchmark_step = b.step("benchmark", "Run Benchmark");
+    benchmark_step.dependOn(&run_benchmark_tests.step);
 }
